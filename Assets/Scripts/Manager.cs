5a44e514d2ceb89b4d0598c6e196342539bd3f62
Christian Schmid <christian.noel.schmid@gmail.com>
2021-07-05 08:31:12 -0700

    Began least cost track impl.

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -46,10 +47,10 @@ namespace Rails
         public MapData Map;
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [SerializeField]
-        private int[,] Tracks = new int[Size, Size];
+        private Dictionary<Vector2Int, int[]> Tracks = new Dictionary<Vector2Int, int[]>();
 
         #endregion
 
@@ -93,6 +94,132 @@ namespace Rails
             return pos;
         }
 
-        #endregion
+        #endregion 
+        
+        /// <summary>
+        /// Inserts a new track onto the Map, based on position and direction.
+        /// </summary>
+        /// <param name="player">The player who owns the track</param>
+        /// <param name="position">The position the track is placed</param>
+        /// <param name="towards">The cardinal direction the track moves towards</param>
+        private void InsertTrack(int player, Vector2Int position, Cardinal towards)
+        {
+            // If Cardinal data doesn't exist for the point yet,
+            // insert and initialize the data
+            if(!Tracks.ContainsKey(position))
+            {
+                Tracks[position] = new int[(int)Cardinal.MAX_CARDINAL];
+                for(int i = 0; i < (int)Cardinal.MAX_CARDINAL; ++i)
+                    Tracks[position][i] = -1;
+            }
+
+            Tracks[position][(int)towards] = player;
+
+            // As Tracks is undirected, insert a track moving the opposite way from the
+            // target node as well.
+            InsertTrack(player, PointTowards(position, towards), ReflectCardinal(towards));
+        }
+
+        /// <summary>
+        /// Returns the point represented by a combined point and cardinal.
+        /// The point returned is the one moving by Cardinal direction, one space over
+        /// from start.
+        /// </summary>
+        /// <param name="start">The start point</param>
+        /// <param name="towards">The cardinal direction moving towards</param>
+        /// <returns>The new point being moved towards.</returns>
+        private Vector2Int PointTowards(Vector2Int start, Cardinal towards)
+        {
+            bool isOdd = start.x % 2 == 1;
+
+            var dir = towards switch
+            {
+                Cardinal.N => Vector2Int.up,
+                Cardinal.NE => isOdd ? new Vector2Int(1, 1) : Vector2Int.right,
+                Cardinal.NW => isOdd ? new Vector2Int(-1, 1) : Vector2Int.left, 
+                Cardinal.S => Vector2Int.down,
+                Cardinal.SW => isOdd ? Vector2Int.left : new Vector2Int(-1, -1),
+                _ => isOdd ? Vector2Int.right : new Vector2Int(1, -1),
+            };
+
+            return start + dir;
+        }
+
+        /// <summary>
+        /// Reflects the given Cardinal, returning the
+        /// exact opposite direction. (ie. N -> S, NE -> SW).
+        /// </summary>
+        /// <param name="towards">The Cardinal being reflected</param>
+        /// <returns>The opposite cardinal direction to towards</returns> 
+        private Cardinal ReflectCardinal(Cardinal towards) =>
+            (Cardinal)Mathf.Repeat((int)Cardinal.SW, (int)towards - 3); 
+    
+        public List<Vector2Int> LeastCostTrack(int player, Vector2Int start, Vector2Int end) 
+        {
+            if(start == end)
+                return new List<Vector2Int> { start };
+            if(!Tracks.ContainsKey(start) || !Tracks.ContainsKey(end))
+                return null;
+
+            // The true distances from start to considered point
+            // (without A* algorithm consideration)
+            var distMap = new Dictionary<Vector2Int, int>();
+            // The list of nodes to return from method
+            var list = new List<Vector2Int>(); 
+            // The list of all nodes visited, connected to their
+            // shortest-path neighbors.
+            var previous = new Dictionary<Vector2Int, Vector2Int>();
+            // The next series of nodes to check
+            var queue = new SortedSet<WeightedNode>();
+            // The current considered node
+            WeightedNode node;
+
+            queue.Add(new WeightedNode (start, 0));
+
+            // While there are still nodes to visit,
+            // Find the lowest-weight one and determine
+            // it's connected nodes.
+            while((node = queue.Min) != null)
+            { 
+                queue.Remove(node); 
+
+                if(node.Position == end)
+                    break;    
+
+                for(Cardinal c = Cardinal.N; c < Cardinal.MAX_CARDINAL; ++c)
+                {
+                    var newPoint = PointTowards(node.Position, c);
+                    if(Tracks.ContainsKey(newPoint))
+                    {
+                        if(distMap.ContainsKey(newPoint))
+                        {
+                            
+                        }
+                        else
+                            queue.Add(new WeightedNode(newPoint, node.Weight + 1));
+                    }
+                } 
+            }
+
+            // If the node's position is the target, a path
+            // was successfully found
+            if(node.Position == end)
+            {
+
+            }
+            else return null;
+        }
+    }
+
+    public class WeightedNode : IComparable<WeightedNode>
+    {
+        public Vector2Int Position { get; set; }
+        public int Weight { get; set; }
+        public WeightedNode(Vector2Int position, int weight)
+        {
+            Position = position;
+            Weight = weight;
+        }
+        public int CompareTo(WeightedNode other) => Weight.CompareTo(other.Weight);
     }
 }
