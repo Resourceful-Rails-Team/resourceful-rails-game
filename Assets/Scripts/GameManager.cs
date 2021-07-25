5f91f7c03de41d75db572451b528afea0b81c42c
Thomas Abel <thomabel@pdx.edu>
2021-07-25 15:28:09 -0700

    train data

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,31 +11,41 @@ namespace Rails {
 		public int train;
 		public int majorcities;
 
-		public PlayerInfo(string name, Color color) {
+		public PlayerInfo(string name, Color color, int money, int train) {
 			this.name = name;
 			this.color = color;
-			money = 0;
-			train = 0;
+			this.money = money;
+			this.train = train;
 			majorcities = 0;
 		}
 	}
 
 	public class GameManager : MonoBehaviour {
 
-		public const int MAXPLAYERS = 2;
-		public const int MAXPHASES = 2;
-		public const int UPGRADE = 20;
-		public const int MAJORCITIES = 6;
-		public const int MONEY = 250;
-		public const int MONEY_START = 50;
+		#region Public Data
+		public int MaxPlayers = 6;
+		// The amount of money each player starts with.
+		public int MoneyStart = 50;
+		// The max amount of money that can be spent building.
+		public int MaxBuild = 20;
+		// The cost to for players to upgrade their train.
+		public int TrainUpgrade = 20;
+		// The number of major cities that must be connected to win.
+		public int Win_MajorCities = 6;
+		// The amount of money needed to win.
+		public int Win_Money = 250;
 
-		#region Data Structures
+		// The trains that players can use.
+		public Rails.ScriptableObjects.TrainData[] trainData;
+		// 
+		public GameObject PlayerInfoPanel;
+		public GameObject[] PhasePanels;
+		#endregion
 
-		public GameObject playerInfo;
-		public GameObject[] uiPanels = new GameObject[MAXPHASES];
-
-		PlayerInfo[] players = new PlayerInfo[MAXPLAYERS];
+		#region Private Data
+		PlayerInfo[] players;
 		Stack<Node> path = new Stack<Node>();
+		int phases;
 		int currentPlayer = 0;
 		int currentPhase = 0;
 		#endregion
@@ -45,14 +55,16 @@ namespace Rails {
 		private void Awake() {
 			// set singleton reference on awake
 			_singleton = this;
-
-			for (int p = 0; p < MAXPLAYERS; p++) {
-				players[p] = new PlayerInfo("Player " + p, Color.white);
-				players[p].money = 50;
-				players[p].train = 0;
-				players[p].majorcities = 0;
-			}
-			uiPanels[currentPhase].SetActive(true);
+			phases = PhasePanels.Length;
+			// Initiate all player info.
+			players = new PlayerInfo[MaxPlayers];
+			for (int p = 0; p < MaxPlayers; p++)
+				players[p] = new PlayerInfo("Player " + p, Color.white, MoneyStart, 0);
+			// Deactivate all panels just in case.
+			for (int u = 0; u < phases; u++)
+				PhasePanels[u].SetActive(false);
+			// Activate first turn panel.
+			PhasePanels[currentPhase].SetActive(true);
 			UpdatePlayerInfo();
 		}
 		#endregion
@@ -82,13 +94,6 @@ namespace Rails {
 
 		#region Player Actions
 
-		// Discards the player's hand.
-		public void DiscardHand() {
-			// TODO: removing and refilling player's hand
-			// Ends the turn.
-			IncrementPlayer();
-			return;
-		}
 		// Moves the train to final node in path.
 		public void MoveTrain() {
 			// TODO: Move train to last pushed node. 
@@ -96,30 +101,26 @@ namespace Rails {
 			UpdatePhase();
 			return;
 		}
+		// Discards the player's hand.
+		public void DiscardHand() {
+			// TODO: removing and refilling player's hand
+			// Ends the turn.
+			IncrementPlayer();
+			return;
+		}
 
-		// Upgrades the player's train.
-		public void UpgradeTrain(int choice) {
-			ref PlayerInfo player = ref players[currentPlayer];
-
-			// If player doesn't have enough money, don't upgrade
-			if (player.money < UPGRADE) {
-				// TODO: Activate failure UI message here.
-				return;
-			}
-
-			// Deduct value from player's money stock and change train value.
-			player.money -= UPGRADE;
-			player.train = choice;
-			Debug.Log(currentPlayer + " $" + players[currentPlayer].money);
-
+		// Builds the track between the nodes in path.
+		public void BuildTrack() {
+			// TODO: Build track between all nodes in stack.
 			// Ends the turn and changes phase.
+			BuildTrack_();
 			IncrementPlayer();
 			UpdatePhase();
 			return;
 		}
-		// Builds the track between the nodes in path.
-		public void BuildTrack() {
-			// TODO: Build track between all nodes in stack.
+		// Upgrades the player's train.
+		public void UpgradeTrain(int choice) {
+			UpgradeTrain_(choice);
 			// Ends the turn and changes phase.
 			IncrementPlayer();
 			UpdatePhase();
@@ -142,35 +143,55 @@ namespace Rails {
 		#endregion
 
 		#region Private Methods
+		// Private method for building.
+		private void BuildTrack_() {
+
+		}
+		// Private method for upgrading.
+		private void UpgradeTrain_(int choice) {
+			ref PlayerInfo player = ref players[currentPlayer];
+
+			// If player doesn't have enough money, don't upgrade
+			if (player.money < TrainUpgrade) {
+				// TODO: Activate failure UI message here.
+				return;
+			}
+
+			// Deduct value from player's money stock and change train value.
+			player.money -= TrainUpgrade;
+			player.train = choice;
+			Debug.Log(currentPlayer + " $" + players[currentPlayer].money);
+		}
+
 		// Changes the current player
 		private int IncrementPlayer() {
 			currentPlayer += 1;
-			if (currentPlayer >= MAXPLAYERS)
+			if (currentPlayer >= MaxPlayers)
 				currentPlayer = 0;
 			UpdatePlayerInfo();
 			return currentPlayer;
 		}
 		// Updates name and money amount. Placeholder.
 		private void UpdatePlayerInfo() {
-			Transform playertext = playerInfo.transform.Find("Player");
+			Transform playertext = PlayerInfoPanel.transform.Find("Player");
 			playertext.GetComponent<TMP_Text>().text = "Player #" + (currentPlayer + 1);
-			playertext = playerInfo.transform.Find("Money");
+			playertext = PlayerInfoPanel.transform.Find("Money");
 			playertext.GetComponent<TMP_Text>().text = "$" + players[currentPlayer].money;
 		}
 		// Cycles through UI screens
 		private int UpdatePhase() {
-			uiPanels[currentPhase].SetActive(false);
+			PhasePanels[currentPhase].SetActive(false);
 			currentPhase += 1;
-			if (currentPhase >= MAXPHASES)
+			if (currentPhase >= phases)
 				currentPhase = 0;
-			uiPanels[currentPhase].SetActive(true);
+			PhasePanels[currentPhase].SetActive(true);
 			return currentPhase;
 		}
 		// Check if the current player has won.
 		private bool CheckWin() {
 			PlayerInfo player = players[currentPlayer];
-			if (player.majorcities < MAJORCITIES ||
-				player.money < MONEY) {
+			if (player.majorcities < Win_MajorCities ||
+				player.money < Win_Money) {
 				return false;
 			}
 			return true;
