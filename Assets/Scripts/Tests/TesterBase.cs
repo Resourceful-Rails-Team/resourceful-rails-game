43a4e2f6213cc114cf7c648188a13f7cd5c01303
Christian Schmid <42004356+cschmid92@users.noreply.github.com>
2021-07-28 13:37:28 -0700

    Created tester and tests for Utilities / PriorityQueue

---
+++ Assets/Scripts/Tests/TesterBase.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace Rails
+{
+    public abstract class TesterBase : MonoBehaviour
+    {
+        // A list of method names
+        private HashSet<string> _methods = new HashSet<string>();
+
+        /// <summary>
+        /// Asserts that the statement given is true. If not, logs an error
+        /// to the console window.
+        /// </summary>
+        /// <param name="statement">The statement tested for veracity.</param>
+        protected void Assert(bool statement)
+        {
+            var trace = new StackTrace();
+            if(!statement)
+            {
+                var method = trace.GetFrame(1).GetMethod();
+
+                _methods.Add(method.Name);
+                UnityEngine.Debug.LogError($"Test Error: method {method} failed.");
+            }
+        }
+        
+        /// <summary>
+        /// Tests the given method, confirming that it contains no
+        /// untrue assertations. Writes an error to the Console window
+        /// if it passed or failed.
+        /// </summary>
+        /// <param name="method">The method to test</param>
+        protected void TestMethod(Action method)
+        {
+            var trace = new StackTrace();
+            var methodName = method.Method.Name;
+
+            method?.Invoke();
+            if (!_methods.Contains(methodName))
+                UnityEngine.Debug.Log($"Test Succeeded: method {methodName}");
+            else
+                UnityEngine.Debug.LogError($"Test Failed: method {methodName}");
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/Tests.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rails
+{
+    public class Tests : TesterBase
+    {
+        private NodeId nodeMid = new NodeId(1, 1);
+        private NodeId nodeN = new NodeId(1, 2);
+        private NodeId nodeNE = new NodeId(2, 2);
+        private NodeId nodeSE = new NodeId(2, 1);
+        private NodeId nodeS = new NodeId(1, 0);
+        private NodeId nodeSW = new NodeId(0, 1);
+        private NodeId nodeNW = new NodeId(0, 2);
+
+        private void Awake()
+        {
+            TestMethod(TestPriorityQueueOrder);
+            TestMethod(TestPriorityQueuePeek); 
+
+            TestMethod(TestReflectCardinal);
+            TestMethod(TestCardinalBetween);
+            TestMethod(TestPointTowards);
+        }
+
+        #region PriorityQueueTests 
+        private void TestPriorityQueueOrder()
+        {
+            var queue = new PriorityQueue<int>();
+            queue.Insert(10);
+            queue.Insert(4);
+            queue.Insert(6);
+            queue.Insert(1);
+            queue.Insert(6);
+
+            Assert(queue.Pop() == 1);
+            Assert(queue.Pop() == 4);
+            Assert(queue.Pop() == 6);
+            Assert(queue.Pop() == 6);
+            Assert(queue.Pop() == 10);
+        }
+        private void TestPriorityQueuePeek()
+        {
+            var queue = new PriorityQueue<string>();
+            queue.Insert("x");
+            queue.Insert("d");
+            queue.Insert("p");
+            queue.Insert("z");
+
+            Assert(queue.Peek() == "d");
+            Assert(queue.Pop() == "d");
+            Assert(queue.Pop() == "p");
+            Assert(queue.Pop() == "x");
+            Assert(queue.Pop() == "z");
+
+            Assert(queue.Peek() == null);
+            Assert(queue.Pop() == null);
+        }
+        #endregion
+
+        #region UtilitiesTests
+        private void TestReflectCardinal()
+        {
+            Assert(Utilities.ReflectCardinal(Cardinal.N) == Cardinal.S);
+            Assert(Utilities.ReflectCardinal(Cardinal.NE) == Cardinal.SW);
+            Assert(Utilities.ReflectCardinal(Cardinal.NW) == Cardinal.SE);
+            Assert(Utilities.ReflectCardinal(Cardinal.S) == Cardinal.N);
+            Assert(Utilities.ReflectCardinal(Cardinal.SW) == Cardinal.NE);
+            Assert(Utilities.ReflectCardinal(Cardinal.SE) == Cardinal.NW);
+        }
+
+        private void TestCardinalBetween()
+        {
+            Assert(Utilities.CardinalBetween(nodeMid, nodeN) == Cardinal.N);
+            Assert(Utilities.CardinalBetween(nodeMid, nodeNE) == Cardinal.NE);
+            Assert(Utilities.CardinalBetween(nodeMid, nodeSE) == Cardinal.SE);
+            Assert(Utilities.CardinalBetween(nodeMid, nodeS) == Cardinal.S);
+            Assert(Utilities.CardinalBetween(nodeMid, nodeSW) == Cardinal.SW);
+            Assert(Utilities.CardinalBetween(nodeMid, nodeNW) == Cardinal.NW);
+        }
+
+        private void TestPointTowards()
+        {
+            Assert(Utilities.PointTowards(nodeMid, Cardinal.N) == nodeN);
+            Assert(Utilities.PointTowards(nodeMid, Cardinal.NE) == nodeNE);
+            Assert(Utilities.PointTowards(nodeMid, Cardinal.SE) == nodeSE);
+            Assert(Utilities.PointTowards(nodeMid, Cardinal.S) == nodeS);
+            Assert(Utilities.PointTowards(nodeMid, Cardinal.SW) == nodeSW);
+            Assert(Utilities.PointTowards(nodeMid, Cardinal.NW) == nodeNW);
+        }
+        #endregion
+
+    }
+}
