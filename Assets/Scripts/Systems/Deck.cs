6c83366732cd83454708bee50e140400045804af
Christian Schmid <42004356+cschmid92@users.noreply.github.com>
2021-07-30 13:18:09 -0700

    Created new Demand class and test implementation.

---
+++ Assets/Scripts/Systems/Deck.cs
@@ -0,0 +1,39 @@
+using Rails.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rails.Systems
+{
+   
+    public static class Deck
+    {
+        // Test data structure
+        // -----------------------
+        private static Demand[][] _demandCards = new Demand[][]
+        {
+            new Demand[]
+            {
+                new Demand (new City { Name = "Portland, OR" }, new Good { Name = "Hazelnuts" }, 100),
+                new Demand (new City { Name = "Boise, ID" },    new Good { Name = "Wheat" },     75),
+                new Demand (new City { Name = "Seattle, WA" },  new Good { Name = "Asparagus" }, 30 ),
+            },
+            new Demand[]
+            {
+                new Demand (new City { Name = "Missoula, MT" },       new Good { Name = "Computer Hardware" },   10),
+                new Demand (new City { Name = "Casper, WY" },         new Good { Name = "Beef" },                100),
+                new Demand (new City { Name = "Salt Lake City, UT" }, new Good { Name = "Integrated Circuits" }, 200),
+            },
+            new Demand[]
+            {
+                new Demand (new City { Name = "Denver, CO" },  new Good { Name = "Apples" }, 10),
+                new Demand (new City { Name = "Redding, CA" }, new Good { Name = "Wine" },   80),
+                new Demand (new City { Name = "Spokane, WA" }, new Good { Name = "Cattle" }, 70),
+            }
+        };
+        // ---------------------
+
+        public static Demand[] DrawOne()
+            => _demandCards[Random.Range(0, _demandCards.Length)];
+    }
+}
