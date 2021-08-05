10eb1ad220c5ff6ea9d968beeb60532671060871
Christian Schmid <42004356+cschmid92@users.noreply.github.com>
2021-08-04 19:41:59 -0700

    Readded UVPan to the board

--- Assets/Scripts/Rendering/UVPan.cs
+++ Assets/Scripts/Rendering/UVPan.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NormalPanner : MonoBehaviour
+public class UVPan : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private const float _padDivisor = 100.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private float _speedX = 1.0f;
+    
+    [SerializeField]
+    private float _speedY = 1.0f;
+
+    private Renderer _renderer;
+    private void Awake() => _renderer = GetComponent<Renderer>();
+    private void Update()
+        => _renderer.material.mainTextureOffset += new Vector2(_speedX, _speedY) / _padDivisor * Time.deltaTime;
 }
+
